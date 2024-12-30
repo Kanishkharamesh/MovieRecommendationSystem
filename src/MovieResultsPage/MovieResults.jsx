588c3ad8952dbaf6f093566b55aed8783a3c67ed
Kanishkharamesh <146187605+Kanishkharamesh@users.noreply.github.com>
2024-12-30 14:44:57 +0530

    Update MovieResults.jsx

--- src/MovieResultsPage/MovieResults.jsx
+++ src/MovieResultsPage/MovieResults.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import './MovieResults.css';
 
-const API_KEY = "5c49b6e2a36066a5b1491648804ef4c1";
+const API_KEY = "YOUR_API_KEY";
 
 const MovieResults = () => {
     const { id } = useParams();
