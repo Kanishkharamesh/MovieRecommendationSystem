1ee61612010a0744940f11f33b56835c17dd9a3d
Kanishkharamesh <146187605+Kanishkharamesh@users.noreply.github.com>
2025-01-06 09:23:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/UserDetails', {
+mongoose.connect('mongodb_url', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
